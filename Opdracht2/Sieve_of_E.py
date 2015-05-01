ddf15c53ebc29aa83c5c6ff6d6f6f2521a0b9241
a4060822 <a.j.nugteren@students.uu.nl>
2015-05-01 17:57:37 +0000

    Prime Sieves

--- Opdracht2/Sieve_of_E.py
+++ Opdracht2/Sieve_of_E.py
@@ -1,39 +1,21 @@
 import time
-import sys
-
+prime= open('prime.dat','w')
 N=int(input('N='))
-t=[x for x in range(3,N,2)]
-priemen=[2]
-if N>=7907:
-    v=[x for x in range(7907,N,2)]
-elif N<7907:
-    v=[]
-prime=open('prime.dat', 'w')
-def Priem1(a):
+t=[x for x in range(2,N)]
+priemen=[]
+def P(a):
     for y in t:
-        if y%a==0:
-            t.remove(y)
-        if y==a:
-            priemen.append(y)
-    for y in v:
-        if y%a==0:
-            v.remove(y)
-    if len(priemen)<999 and len(t)>0:
-        Priem1(t[0])
-def Priem2(a):
-    for y in v:
-        if y%a==0:
-            v.remove(y)
-        if y==a:
-            priemen.append(y)
-    if len(v)>0:
-        Priem2(v[0])
-T1 = time.perf_counter()
-Priem1(3)
-if v!=[]:
-    Priem2(v[0])
-T2 = time.perf_counter()
-Time ='Time required '+ str(T2-T1)+ ' sec.'
-print('$ python3 Sieve_of_E.py ', str(int(N)), ' prime.dat Found ', str(len(priemen)), ' Prime numbers smaller than ', str(int(N)), ' in '+ str(Time)+ ' sec.') 
+        if y!=0:
+            veelv=range(y,N,y)
+            for f in veelv[1:N]:
+                if t[f-2]!=0:
+                    t[f-2]=0
+    for z in t:
+        if z!=0:
+            priemen.append(z)
+T1=time.perf_counter()
+P(N)
+T2=time.perf_counter()
+Time=str(T2-T1)
+print(str(len(priemen)), ' Prime numbers smaller than ', str(N), ' in ', Time, ' sec.')
 prime.write('\n'.join(map(str, priemen)))
-prime.close()
