22d31d848704cc1fb9ad402877d57797bfe4565e
a4060822 <a.j.nugteren@students.uu.nl>
2015-05-11 21:59:09 +0000

    Estimating Pi

--- Opdracht3/estimate_pi.py
+++ Opdracht3/estimate_pi.py
@@ -1,37 +1,81 @@
 import random
 import sys
 import math
-def drop_needle(L):
-    x=random.random()
-    a=random.vonmisesvariate(0,0)
-    if L<=1:
-        if x+L*math.cos(a)<=0 or x+L*math.cos(a)>=1 or x==0 or x==1:
-            z=True
-        else:
-            z=False
+def drop_needle(L,M):
+    x=t[M]
+    a=s[M]
+    if x+L*math.cos(a)<=0:
+        z=True
+    elif x+L*math.cos(a)>=1:
+        z=True
+    elif x==0:
+        z=True
+    elif x==1:
+        z=True
+    else:
+        z=False
     return z
 def recurse(N):
     if N>0:
-        y=drop_needle(L)
+        y=drop_needle(L,N-1)
         if y==True:
-            t.append(y)
-        x=random.random()
-        a=random.vonmisesvariate(0,0)
+            f.append(y)
         recurse(N-1)
-        return t
-L=float(sys.argv[2])
-N=int(sys.argv[1])
-t=[]
+        return f
+f=[]
 if len(sys.argv)<=2:
-    print('Use:  estimate_pi.py N L')
+    print('Use: ',sys.argv[0],' N L')
 if len(sys.argv)==3:
+    L=float(sys.argv[2])
+    N=int(sys.argv[1])
     if float(sys.argv[2])>1:
-        print('AssertionError: L should be smaller than 1')
-    if float(sys.argv[2])<=1:
+        t=[x for x in range(0,N)]
+        s=[x for x in range(0,N)]
+        for i in range(0,N):
+            t[i]=random.random()
+            s[i]=random.vonmisesvariate(0,0)
         recurse(N)
-        h=len(t)
+        h=len(f)
+        Pi= (2*L-2*(math.sqrt((L**2)-1)+math.asin(1/L)))/((h/N)-1)
+        print(str(h), ' hits in ' , sys.argv[1], ' tries ')
+        print('Pi = ', str(Pi))
+    if float(sys.argv[2])<=1:
+        t=[x for x in range(0,N)]
+        s=[x for x in range(0,N)]
+        for i in range(0,N):
+            t[i]=random.random()
+            s[i]=random.vonmisesvariate(0,0)
+        recurse(N)
+        h=len(f)
         Pi= (2*L*N)/h
         print(str(h), ' hits in ' , sys.argv[1], ' tries ')
         print('Pi = ', str(Pi))
-
-        
+if len(sys.argv)==4:
+    L=float(sys.argv[2])
+    N=int(sys.argv[1])
+    if float(sys.argv[2])>1:
+        seed=int(sys.argv[3])
+        random.seed(seed)
+        t=[x for x in range(0,N)]
+        s=[x for x in range(0,N)]
+        for i in range(0,N):
+            t[i]=random.random()
+            s[i]=random.vonmisesvariate(0,0)
+        recurse(N)
+        h=len(f)
+        Pi= (2*L-2*(math.sqrt((L**2)-1)+math.asin(1/L)))/((h/N)-1)
+        print(str(h), ' hits in ' , sys.argv[1], ' tries ')
+        print('Pi = ', str(Pi))
+    if float(sys.argv[2])<=1:
+        seed=int(sys.argv[3])
+        random.seed(seed)
+        t=[x for x in range(0,N)]
+        s=[x for x in range(0,N)]
+        for i in range(0,N):
+            t[i]=random.random()
+            s[i]=random.vonmisesvariate(0,0)
+        recurse(N)
+        h=len(f)
+        Pi= (2*L*N)/h
+        print(str(h), ' hits in ' , sys.argv[1], ' tries ')
+        print('Pi = ', str(Pi))
