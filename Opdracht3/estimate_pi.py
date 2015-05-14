e9bd1592e5076a6b10fa3c2a0967d88381853264
a4060822 <a.j.nugteren@students.uu.nl>
2015-05-14 10:41:31 +0000

    Estimating Pi

--- Opdracht3/estimate_pi.py
+++ Opdracht3/estimate_pi.py
@@ -4,38 +4,30 @@ import math
 def drop_needle(L,M):
     x=t[M]
     a=s[M]
-    if x+L*math.cos(a)<=0:
-        z=True
-    elif x+L*math.cos(a)>=1:
-        z=True
-    elif x==0:
-        z=True
-    elif x==1:
+    if x+L*math.cos(a)<=0 or x+L*math.cos(a)>=1 or x==0 or x==1:
         z=True
     else:
         z=False
-    return z
-def recurse(N):
-    if N>0:
-        y=drop_needle(L,N-1)
-        if y==True:
-            f.append(y)
-        recurse(N-1)
-        return f
-f=[]
+    f[M]=z
+g=[]
 if len(sys.argv)<=2:
     print('Use: ',sys.argv[0],' N L')
 if len(sys.argv)==3:
     L=float(sys.argv[2])
     N=int(sys.argv[1])
+    f=[x for x in range(0,N)]
     if float(sys.argv[2])>1:
         t=[x for x in range(0,N)]
         s=[x for x in range(0,N)]
         for i in range(0,N):
             t[i]=random.random()
             s[i]=random.vonmisesvariate(0,0)
-        recurse(N)
-        h=len(f)
+        for j in range(0,N):    
+            drop_needle(L,j)
+        for z in f:
+            if z!=False:
+                g.append(z)
+        h=len(g)
         Pi= (2*L-2*(math.sqrt((L**2)-1)+math.asin(1/L)))/((h/N)-1)
         print(str(h), ' hits in ' , sys.argv[1], ' tries ')
         print('Pi = ', str(Pi))
@@ -45,14 +37,19 @@ if len(sys.argv)==3:
         for i in range(0,N):
             t[i]=random.random()
             s[i]=random.vonmisesvariate(0,0)
-        recurse(N)
-        h=len(f)
+        for j in range(0,N):    
+            drop_needle(L,j)
+        for z in f:
+            if z!=False:
+                g.append(z)
+        h=len(g)
         Pi= (2*L*N)/h
         print(str(h), ' hits in ' , sys.argv[1], ' tries ')
         print('Pi = ', str(Pi))
 if len(sys.argv)==4:
     L=float(sys.argv[2])
     N=int(sys.argv[1])
+    f=[x for x in range(0,N)]
     if float(sys.argv[2])>1:
         seed=int(sys.argv[3])
         random.seed(seed)
@@ -61,8 +58,12 @@ if len(sys.argv)==4:
         for i in range(0,N):
             t[i]=random.random()
             s[i]=random.vonmisesvariate(0,0)
-        recurse(N)
-        h=len(f)
+        for j in range(0,N):    
+            drop_needle(L,j)
+        for z in f:
+            if z!=False:
+                g.append(z)
+        h=len(g)
         Pi= (2*L-2*(math.sqrt((L**2)-1)+math.asin(1/L)))/((h/N)-1)
         print(str(h), ' hits in ' , sys.argv[1], ' tries ')
         print('Pi = ', str(Pi))
@@ -74,8 +75,12 @@ if len(sys.argv)==4:
         for i in range(0,N):
             t[i]=random.random()
             s[i]=random.vonmisesvariate(0,0)
-        recurse(N)
-        h=len(f)
+        for j in range(0,N):    
+            drop_needle(L,j)
+        for z in f:
+            if z!=False:
+                g.append(z)
+        h=len(g)
         Pi= (2*L*N)/h
         print(str(h), ' hits in ' , sys.argv[1], ' tries ')
         print('Pi = ', str(Pi))
